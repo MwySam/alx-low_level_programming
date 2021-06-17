81a5a8273f00d2b5d7ed59e235631427ef176b91
MwySam <samuelmwy@gmail.com>
2021-06-17 12:14:04 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 the function returns nothing
+ */
+int main(void) 
+{
+  /*
+   * prints the required text
+   */
+  printf("with proper grammar, but the outcome is a piece of art,\n"); 
+  return (0);
+}
