4ff7da850e3768c0e9f6cf5c5ace2a8eebb1ba8e
MwySam <samuelmwy@gmail.com>
2021-06-18 12:59:54 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - program entry point
+* Return: 0 returns nothing
+*/
+int main(void)	
+{	
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
