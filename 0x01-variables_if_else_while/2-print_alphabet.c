0aa619f53d7e6990717f9c8cc7d452504a929233
MwySam <samuelmwy@gmail.com>
2021-06-18 12:31:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**         
+* main - entry for  c coding challenge
+* in low level coding section
+* Return: 0 returns zero
+*/
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);	
+}        
