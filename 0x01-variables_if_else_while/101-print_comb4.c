e7df1eaeb65316849b254674e9f66196bb6c937d
MwySam <samuelmwy@gmail.com>
2021-06-21 13:00:06 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Description: print all possible different
-*            combinations of two digits.
-*
+* main - prints all possible different combinations of three digits
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-int a;
-int b;
-int c;
-/*print first digit*/
-for (a = 0; a < 10; a++)
+int n, m, l;
+
+for (n = 48; n < 58; n++)
 {
-/*print second digit*/
-for (b = 0; b < 10; b++)
+for (m = 49; m < 58; m++)
 {
-/*print third digit*/
-for (c = 0; c < 10; c++)
+for (l = 50; l < 58; l++)
 {
-/**
-* don't print three matching digits and
-* make sure first digit is less than
-* the second digit and the second digit is
-* less than the third digit.
-*/
-if (a != b && a < b && b != c && b < c)
+if (l > m && m > n)
 {
-putchar(a + '0');
-putchar(b + '0');
-putchar(c + '0');
-/* dont place a comma and space after 789*/
-if (a + b + c != 24)
+putchar(n);
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
 {
 putchar(',');
 putchar(' ');
