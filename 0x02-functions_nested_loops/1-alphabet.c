87712fc976eb1a21623538fa0aa8907aaeae8c1e
MwySam <samuelmwy@gmail.com>
2021-06-22 02:14:48 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * main - check the code for ALX School students.
+ *
+ *Description:function called print_alphabet
+ *            that utilizes on the _putchar
+ *            function prototype
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch ='a'; ch <= z; ch++)
+{
+_putchar ("ch \n");
+}
+}
+int main (void)
+{
+print_alphabet(void);
+return (0)
+}
