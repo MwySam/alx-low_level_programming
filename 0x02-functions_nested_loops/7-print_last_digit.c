ebdbd0014492b127cdbc5fe97b55f185be258f04
MwySam <samuelmwy@gmail.com>
2021-06-22 11:30:55 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,17 +4,17 @@
  * @i: integer input argument.
  * Return: returns last digit of input
  */
-int print_last_digit(int i)	
-{	
-int lastDigit;
-if (i < 0)		
+int print_last_digit(int i)
 {
-lastDigit = (i % 10) * -1;		
-}	
-else		
-{		
+int lastDigit;
+if (i < 0)
+{
+lastDigit = (i % 10) * -1;
+}
+else
+{
 lastDigit = i % 10;
 }
 _putchar(lastDigit + '0');
-return (lastDigit);	
+return (lastDigit);
 }
