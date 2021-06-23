98a84a7b013243ec53195f7f22dba4a27ab1e9c7
MwySam <samuelmwy@gmail.com>
2021-06-23 12:08:14 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,14 +4,21 @@
  *
  * Return: Always 0.
  */
-int main(void)	
+int main(void)
 {
 print_times_table(-2);
+{
 _putchar('\n');
 print_times_table(5);
+{
 _putchar('\n');
 print_times_table(98);
+{
 _putchar('\n');
+
 print_times_table(12);
+}
+}
+}
 return (0);	
 }
