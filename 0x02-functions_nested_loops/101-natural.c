123627bfa450986ee69c761b3eef78c5567ba182
MwySam <samuelmwy@gmail.com>
2021-06-23 12:35:53 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,8 +5,8 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)	
-{	
+int main(void)
+{
 unsigned long int sum3, sum5, sum;
 int i;
 sum3 = 0;
@@ -17,7 +17,8 @@ for (i = 0; i < 1024; ++i)
 if ((i % 3) == 0)
 {
 sum3 = sum3 + i;
-} else if ((i % 5) == 0)
+}
+else if ((i % 5) == 0)
 {
 sum5 = sum5 + i;
 }
