80c8462b17ba463421a7c234c77dfaa08f893131
MwySam <samuelmwy@gmail.com>
2021-06-24 09:41:30 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_most_numbers - prints numerical digits except 2 and 4.
  * Return: 0 - doesn't return anything.
  */
 void print_most_numbers(void)
 {
-char c = '0';
-if (c != '2' && c != '4')
+int i = '0';
+if (i != '2' && i != '4')
 {
-while (c <= '9')
+while (i <= '9')
 {
-_putchar(c);
-c++;
+_putchar(i);
+i++;
 }
 }
 _putchar('\n');
