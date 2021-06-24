eb3f79aba2daf216970fb0fce00eb1c8881c4d7c
MwySam <samuelmwy@gmail.com>
2021-06-24 02:55:23 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints numerical digits except 2 and 4.
+ * Return: 0 - doesn't return anything.
+ */
+void print_most_numbers(void)
+{
+char c = '0';
+while (c <= '9' && c != '2' && c != 4)
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
