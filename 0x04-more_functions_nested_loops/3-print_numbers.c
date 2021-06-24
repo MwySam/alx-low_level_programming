708b5a8becbd1ea6d42804bbfef4494fb4fb51c4
MwySam <samuelmwy@gmail.com>
2021-06-24 02:30:59 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints numerical digits.
+ * Return: 0 - doesn't return anything.
+ */
+void print_numbers(void)
+{
+char c = '0';
+while (c <= '9')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
