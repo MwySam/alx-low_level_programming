7cbabd99e6a46d699b9854c1c0b25f0f62826ade
MwySam <samuelmwy@gmail.com>
2021-06-24 10:07:45 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,11 @@
 #include "holberton.h"
-
 /**
  * print_most_numbers - Print numbers between 0 to 9 incl.
  *
  * Return: Void.
  */
-void print_most_numbers(void)	
-{	
+void print_most_numbers(void)
+{
 int i;
 for (i = 0; i < 10; i++)
 {
@@ -15,5 +14,5 @@ if (i != 2 && i != 4)
 _putchar(i + '0');
 }
 }
-_putchar('\n');	
+_putchar('\n');
 }
