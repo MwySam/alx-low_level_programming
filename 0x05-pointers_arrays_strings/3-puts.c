77c24e3dd525400f2f594ad426f0ca3b9f29bda9
MwySam <samuelmwy@gmail.com>
2021-06-28 05:07:49 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _puts -  prints a string, followed by a new line
+ * @s: pointer
+ * Return: length of string
+ */
+void _puts(char *str)
+{
+char t = *str;
+puts ("t\n");
+}
