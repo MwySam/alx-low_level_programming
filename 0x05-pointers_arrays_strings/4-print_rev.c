4e947a540f571ad5d5894e7fdc478fda9a10633e
MwySam <samuelmwy@gmail.com>
2021-06-28 06:22:01 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+
+ * print_rev - print script in reverse
+ * @str: pointer to the string to print
+ * Return: void
+ */
+void print_rev(char *s)
+	void print_rev(char *s)
+	
+{
+int i = 0;	
+while (s[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
