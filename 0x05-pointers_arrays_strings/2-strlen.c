d207a450c51ca9901e7a29aab99de8dfd9487877
MwySam <samuelmwy@gmail.com>
2021-06-28 04:25:09 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * int _strlen -  returns the length of a string
+ * @s: pointer
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int t;
+t = _strlen (*s);
+return t;
+}
