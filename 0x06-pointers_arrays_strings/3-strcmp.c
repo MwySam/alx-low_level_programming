9c84c759448984324dd11494cb5a26355af6f635
MwySam <samuelmwy@gmail.com>
2021-07-01 12:12:45 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,21 +3,21 @@
 * _strcmp - compares two strings
 * @s1: string 1
 * @s2: string 2
-* Return: 0 if matching, and ns1 - ns2 if not matching
+* Return: 1 if true, 0 if false
 */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-/**
-* We have reached the end of S1 OR we have reached the end of S2
-* 
-*/
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+int equal = 0;
+
+while (*s1)
 {
-if (s1[i] != s2[i])
+if (*s1 != *s2)
 {
-return (s1[i] - s2[i]);
+equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+break;
 }
+s1++;
+s2++;
 }
-return (0);
+return (equal);
 }
