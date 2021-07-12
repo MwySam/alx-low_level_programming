297ec2d09f58438de929f92f97395209e88fff8f
MwySam <samuelmwy@gmail.com>
2021-07-12 12:15:44 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
-* main - prints program name, followed by a new line
-* @argc: int
-* @argv: list
-* Return: 0
+* main - prints the name of the program
+* @argc : counts how many arguments are passed to the program which is unused
+* for this program.
+* @argv : stores the arguments passed to the program in an
+* array of size argv[argc-1]
+* Return: an interger 0 for sucess.
 */
-
-int main(int argc, char const *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void)argc;
 printf("%s\n", argv[0]);
 return (0);
 }
