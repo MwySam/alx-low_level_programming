1ceeea5ce46f81dd0a7ee0cc353b6d6d4d37eaf5
MwySam <samuelmwy@gmail.com>
2021-07-15 13:41:54 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,38 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
-* _strlen - return length of string
-*
-* @s: string to count
-*
-* Return: the size
-*/
-int _strlen(char *s)
-{
-int counter = 0;
-
-while (*s != 0)
-{
-counter++;
-s++;
-}
-return (counter);
-}
-/**
-* *string_nconcat - concatenates two strings
-* @s1: first string
-* @s2: second string
-* @n: number of bytes
-* Return: pointer to new space in memory that contains s1 + n bytes of s2
+**string_nconcat - a function that concatenates two strings.
+*@s1 : pointer to string
+*@s2 : pointer to string
+*@n : integer
+*Return: pointer to string
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int l1, l2, i, j;
-char *p;
-l1 = s1 != NULL ? _strlen(s1) : 0;
-l2 = s2 != NULL ? _strlen(s2) : 0;
-if (n >= l2)
-n = l2;
-p = malloc((l1 + n) * sizeof(char) + 1);
-if (p == NULL)
+
+
+
+unsigned int x1, x2, i, j;
+char *s;
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (x1 = 0; s1[x1] != '\0'; x1++)
+;
+for (x2 = 0; s2[x2] != '\0'; x2++)
+;
+if (n >= x2)
+n = x2;
+s = malloc(sizeof(char) * (x1 + n + 1));
+if (s == NULL)
 return (NULL);
-for (i = 0; i < l1; i++)
-{
-p[i] = s1[i];
-}
-for (j = 0; j < n && s2[j] != '\0'; j++, i++)
-p[i] = s2[j];
-p[i] = 0;
-return (p);
+for (i = 0; s1[i] != '\0'; i++)
+s[i] = s1[i];
+for (j = x1; j < x1 + n; j++)
+s[j] = s2[j - x1];
+s[j] = '\0';
+return (s);
 }
