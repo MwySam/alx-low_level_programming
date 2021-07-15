7744644dc34293b29c482e8fe3c5a07dc6ada3e3
MwySam <samuelmwy@gmail.com>
2021-07-15 12:17:29 -0700

    holberton.h update

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-void _puts(char *str);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -9,7 +9,4 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
-int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
 #endif
