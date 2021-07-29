46cc84bedb7d1c95a5e541d5c97cab0671e2d798
MwySam <samuelmwy@gmail.com>
2021-07-29 13:27:50 -0700

    task 2 and fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,8 +16,9 @@ typedef struct list_s
 {
 char *str;
 unsigned int len;
-struct list_s *next;	
-} list_t;
+struct list_s *next;
+}
+list_t;
 /* function prototypes for all files */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
